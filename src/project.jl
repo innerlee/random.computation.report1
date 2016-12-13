47ed08fa4600ce21208720a1cec60497bc2b0d94
lizz <363664470@qq.com>
2016-12-13 12:42:43 +0800

    op

--- src/project.jl
+++ src/project.jl
@@ -3,8 +3,45 @@ using JLD
 using HDF5
 using DataFrames
 
-# data preprocess
-include("preprocess_movie.jl")
+"""
+    movie_process()
+
+Preprocess MovieLens 20M dataset
+Convert the raw rating data to a sparse matrix.
+- Rows are movies and columns are users.
+- Elements are ratings.
+"""
+function movie_process()
+    # read raw data 20000263×4. [userId │ movieId │ rating │ timestamp]
+    df = readtable("data/ml-20m/ratings.csv")
+    # user ids are continuous.
+    assert(length(unique(df[:userId])) == maximum(df[:userId]))
+    # compress the movie ids. 26744 movies with maximum id 131262.
+    mids = sort(unique(df[:movieId]))
+    mid_compress = Dict()
+    mid_recover = Dict()
+    for (i, mid) in enumerate(mids)
+        mid_compress[mid] = i
+        mid_recover[i] = mid
+    end
+    # build sparse matrix. row: movie, col: user, elem: rating
+    # 26744×138493 sparse matrix with 20000263 Float64 nonzero entries
+    sparse([mid_compress[i] for i in df[:movieId]], df[:userId], df[:rating])
+end
+
+"""
+    save_process_data()
+
+Save the ratings sparse matrix to a file.
+"""
+save_process_data() = save("output/movielens20m.jld", "rating", movie_process())
+
+"""
+    load_movielens()
+
+Load the rating sparse matrix.
+"""
+load_movielens() = load("output/movielens20m.jld")["rating"]
 
 """
     test_svd()
@@ -64,7 +101,7 @@ function bench_k(krange=10:10:100)
         toc()
         # compute diff
         dif = vecnorm(s0[k+1:end])
-        dif2 = norm(full(A) - (ss[1].U * spdiagm(ss[1].S)) * ss[1].Vt')
+        dif2 = maxeig(full(A) - (ss[1].U * spdiagm(ss[1].S)) * ss[1].Vt')
         println("||A-A_k||_F = ", round(dif,1))
         println("||A-A_k||_2 = ", round(dif2,1))
     end
@@ -150,12 +187,33 @@ function bench_sketch_frob(repeat=1, tt=[256])
 end
 
 """
+    maxeig(A)
 
-Subspace power method
+Compute maximum eig of A by power method.
 """
-function sub_power(fA; k=128, q=10)
+function maxeig(A; iter=16)
+    v = normalize(randn(size(A, 2)))
+    for i = 1:iter
+        v = normalize(A*v)
+        v = normalize(A'*v)
+    end
+    norm(A * v)
+end
+
+"""
+    sub_power(fA; k=128, q=4)
+
+Argumeents
+    fA  : full matrix A
+    k   : rank
+    q   : power
+
+Low rank approx. w.r.t. operator norm by subspace power method.
+Error is evaluated by power method.
+"""
+function sub_power(fA; k=128, q=4)
     tic()
-        n = size(A, 2)
+        n = size(fA, 2)
         G = randn(n, k)
         Y = fA * G
         for i = 1:q
@@ -163,9 +221,11 @@ function sub_power(fA; k=128, q=10)
             Y = fA * Y
         end
         Z, _ = qr(Y)
+        # the approximation
+        Ak = Z * (Z' * fA)
     time_power = toq()
-    Aa = Z * (Z' * fA);
-    @time err = norm(fA - A)
+    # compute error using power method
+    err = maxeig(fA - Ak)
     (Z, err, time_power)
 end
 
@@ -178,41 +238,40 @@ Arguments
 
 sketch benchmark for the operator norm.
 """
-function bench_sub_power(repeat=1, qq=[256])
+function bench_sub_power(repeat=1, qq=[4])
     # 138493 × 26744, did a transpose for this shape.
     A = load_movielens()'
     fA = full(A)
     t0 = 111.4
-    e0 = 11622.2
-    for t in qq
-        println("> will reduce A with size $(size(A)) to $t rows")
+    e0 = 424.3
+    for q in qq
+        println("> will reduce A with size $(size(A)) to $q rows")
         results = []
         for i = 1:repeat
-            r = sub_power(A, fA, q=q)
+            r = sub_power(fA, q=q)
             push!(results, r)
         end
 
-        # usv               = [r[1]    for r in results]
-        # err               = [r[2]    for r in results]
-        # time_sketch       = [r[3][1] for r in results]
-        # time_tiny_problem = [r[3][2] for r in results]
+        zzz               = [r[1] for r in results]
+        err               = [r[2] for r in results]
+        time_power        = [r[3] for r in results]
 
-        # println("repeat = ", repeat)
-        # println("mean time spent = ", mean(time_sketch + time_tiny_problem))
-        # println("          ratio = ", mean(time_sketch + time_tiny_problem) / t0)
-        # println("    sketch: ", mean(time_sketch))
-        # println("    tiny problem: ", mean(time_tiny_problem))
-        # println("min/max/median/mean/std err (frob norm): ", round([
-        #     minimum(err), maximum(err), median(err), mean(err), std(err)
-        #     ], 4))
-        # println("min err (frob norm) = ", minimum(err))
-        # println("              ratio = ", minimum(err) / e0)
+        println("repeat = ", repeat)
+        println("mean time spent = ", mean(time_power))
+        println("          ratio = ", mean(time_power) / t0)
+        println("min/max/median/mean/std err (op norm): ", round([
+            minimum(err), maximum(err), median(err), mean(err), std(err)
+            ], 4))
+        println("min err (op norm) = ", minimum(err))
+        println("            ratio = ", minimum(err) / e0)
     end
 end
 
-## main
+## config
 REPEAT = 100
 TT     = [256, 512, 1024]
+QQ     = [8, 16, 32]
 
-# bench_sketch_frob(REPEAT, TT)
-bench_sub_power(1, [256])
+# main
+bench_sketch_frob(REPEAT, TT)
+bench_sub_power(REPEAT, QQ)
